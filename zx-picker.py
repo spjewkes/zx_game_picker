65e4f5c339971c7b6f3dd5a5aecb727252b1aa75
Steve Jewkes <steve@mulus.net>
2024-04-02 16:53:45 +0100

    Got genre picker tidied up

--- zx-picker.py
+++ zx-picker.py
@@ -10,6 +10,7 @@ def main() -> int:
     parser.add_argument('file', metavar='FILE', type=str, nargs='?', default='ZXDB_sqlite.db', help='Override ZXDB file to use (in SQLite format)')
     parser.add_argument('--list-genres', dest='list_genres', action=argparse.BooleanOptionalAction, help='Lists all available genres along with their IDs')
     parser.set_defaults(list_genres=False)
+    parser.add_argument('--genres', dest='genres', nargs='+', type=int, help='List of genres to select from') 
     args = parser.parse_args()
 
     con = sqlite3.connect(args.file)
@@ -21,10 +22,21 @@ def main() -> int:
         print("---- --------------------------------------------------")
         for _id, _genre in res.fetchall():
             print(f"{_id:<4} {_genre:<50}")
-        # print(res.fetchall())
     else:
-        res = cur.execute("SELECT title FROM entries ORDER BY RANDOM() LIMIT 1;")
-        print(res.fetchall()[0][0])
+        fetch_string = "SELECT title FROM entries "
+        if args.genres:
+            fetch_string += "WHERE "
+
+        if args.genres:
+            fetch_string += "genretype_id IN (" + ",".join(map(str, args.genres)) + ") "
+
+        fetch_string += "ORDER BY RANDOM() LIMIT 1;"
+
+        # print(fetch_string)
+        res = cur.execute(fetch_string)
+        # print(res.fetchall())
+        title, = res.fetchall()[0]
+        print(f"Title: {title}")
 
     con.close()
 
