0db3c72c3b4d74412b69617ec92b108b51409892
Steve Jewkes <steve@mulus.net>
2024-04-02 17:17:05 +0100

    Added extra genre shortcuts

--- zx-picker.py
+++ zx-picker.py
@@ -10,8 +10,15 @@ def main() -> int:
     parser.add_argument('file', metavar='FILE', type=str, nargs='?', default='ZXDB_sqlite.db', help='Override ZXDB file to use (in SQLite format)')
     parser.add_argument('--list-genres', dest='list_genres', action=argparse.BooleanOptionalAction, help='Lists all available genres along with their IDs')
     parser.set_defaults(list_genres=False)
-    parser.add_argument('--genres', dest='genres', nargs='+', type=int, help='List of genres to select from') 
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--genres', dest='genres', nargs='+', type=int, help='List of genres to select from') 
+    group.add_argument('--arcade', action='store_true', help='Select from all genres that are arcade games')
+    group.add_argument('--adventure', action='store_true', help='Select from all generes that are adventure games')
+    group.add_argument('--misc', action='store_true', help='Select from games not flagged as either adventure or arcade')
+    group.add_argument('--all', action='store_true', help='Select from all games')
     args = parser.parse_args()
+
+    genre_enabled = args.genres or args.arcade or args.adventure or args.misc or args.all
 
     con = sqlite3.connect(args.file)
     cur = con.cursor()
@@ -24,11 +31,21 @@ def main() -> int:
             print(f"{_id:<4} {_genre:<50}")
     else:
         fetch_string = "SELECT title FROM entries "
-        if args.genres:
+        if genre_enabled:
             fetch_string += "WHERE "
 
+        genres = list()
         if args.genres:
-            fetch_string += "genretype_id IN (" + ",".join(map(str, args.genres)) + ") "
+            genres.extend(args.genres)
+        if args.adventure or args.all:
+            genres.extend([1, 2, 4, 5, 6])
+        if args.arcade or args.all:
+            genres.extend([7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+        if args.misc or args.all:
+            genres.extend([27, 28, 29, 31, 32, 17, 18, 19, 30, 20, 21, 26])
+
+        if genre_enabled:
+            fetch_string += "genretype_id IN (" + ",".join(map(str, genres)) + ") "
 
         fetch_string += "ORDER BY RANDOM() LIMIT 1;"
 
