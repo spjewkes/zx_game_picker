b9a7f1c127e0163ba1f45a17749bb5337b17e6be
Steve Jewkes <steve@mulus.net>
2024-04-02 16:25:22 +0100

    Added an option to list genres

--- zx-picker.py
+++ zx-picker.py
@@ -7,17 +7,20 @@ import sqlite3
 def main() -> int:
     """ Main entrypoint """
     parser = argparse.ArgumentParser(description='Utility for randomly picking games from the ZXDB.')
-    parser.add_argument('file', metavar='FILE', type=str, help='The ZXDB file to use (in SQLite format)')
-
+    parser.add_argument('file', metavar='FILE', type=str, nargs='?', default='ZXDB_sqlite.db', help='Override ZXDB file to use (in SQLite format)')
+    parser.add_argument('--list-genres', dest='list_genres', action=argparse.BooleanOptionalAction, help='Lists all available genres along with their IDs')
+    parser.set_defaults(list_genres=False)
     args = parser.parse_args()
 
     con = sqlite3.connect(args.file)
-
     cur = con.cursor()
 
-    res = cur.execute("SELECT title FROM entries ORDER BY RANDOM() LIMIT 1;")
-
-    print(res.fetchall()[0][0])
+    if args.list_genres:
+        res = cur.execute("SELECT * FROM genretypes;")
+        print(res.fetchall())
+    else:
+        res = cur.execute("SELECT title FROM entries ORDER BY RANDOM() LIMIT 1;")
+        print(res.fetchall()[0][0])
 
     con.close()
 
