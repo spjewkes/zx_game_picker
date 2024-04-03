541c09e7042a9ea6bc707ee74bb9c8ad22f283e8
Steve Jewkes <steve@mulus.net>
2024-04-03 10:38:51 +0100

    Improved date selection

--- zx-picker.py
+++ zx-picker.py
@@ -7,16 +7,24 @@ import sqlite3
 def main() -> int:
     """ Main entrypoint """
     parser = argparse.ArgumentParser(description='Utility for randomly picking games from the ZXDB.')
+
     parser.add_argument('file', metavar='FILE', type=str, nargs='?', default='ZXDB_sqlite.db', help='Override ZXDB file to use (in SQLite format)')
     parser.add_argument('--list-genres', dest='list_genres', action=argparse.BooleanOptionalAction, help='Lists all available genres along with their IDs')
     parser.set_defaults(list_genres=False)
-    parser.add_argument('--years', dest='years', nargs='+', type=int, help='List of years to select from')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--genres', dest='genres', nargs='+', type=int, help='List of genres to select from') 
-    group.add_argument('--arcade', action='store_true', help='Select from all genres that are arcade games')
-    group.add_argument('--adventure', action='store_true', help='Select from all generes that are adventure games')
-    group.add_argument('--misc', action='store_true', help='Select from games not flagged as either adventure or arcade')
-    group.add_argument('--all', action='store_true', help='Select from all games')
+
+    genre_group = parser.add_mutually_exclusive_group()
+    genre_group.add_argument('--genres', dest='genres', nargs='+', type=int, help='List of genres to select from') 
+    genre_group.add_argument('--arcade', action='store_true', help='Select from all genres that are arcade games')
+    genre_group.add_argument('--adventure', action='store_true', help='Select from all generes that are adventure games')
+    genre_group.add_argument('--misc', action='store_true', help='Select from games not flagged as either adventure or arcade')
+    genre_group.add_argument('--all', action='store_true', help='Select from all games')
+
+    year_group = parser.add_mutually_exclusive_group()
+    year_group.add_argument('--years', dest='years', nargs='+', type=int, help='List of years to select from')
+    year_group.add_argument('--1980s', dest='the80s', action='store_true', help='Select entries from the 1980s')
+    year_group.add_argument('--1990s', dest='the90s', action='store_true', help='Select entries from the 1990s')
+    year_group.add_argument('--modern', dest='modern', action='store_true', help='Selectr entries from the 2000s and onwards')
+
     args = parser.parse_args()
 
     genre_enabled = args.genres or args.arcade or args.adventure or args.misc or args.all
@@ -47,16 +55,25 @@ def main() -> int:
             genres.extend([27, 28, 29, 31, 32, 17, 18, 19, 30, 20, 21, 26])
 
         if genre_enabled:
-            where_clauses.append("entries.genretype_id IN (" + ",".join(map(str, genres)) + ") ")
+            where_clauses.append("entries.genretype_id IN (" + ",".join(map(str, genres)) + ")")
 
         # Select years (if defined)
         if args.years:
-            where_clauses.append("releases.release_year IN (" + ",".join(map(str, args.years)) + ") ")
+            where_clauses.append("releases.release_year IN (" + ",".join(map(str, args.years)) + ")")
+
+
+        if args.the80s:
+            where_clauses.append("releases.release_year >= 1980 AND releases.release_year <= 1989")
+        if args.the90s:
+            where_clauses.append("releases.release_year >= 1990 AND releases.release_year <= 1999")
+        if args.modern:
+            where_clauses.append("releases.release_year >= 1990")
 
         # Add where clause (if defined)
         if where_clauses:
             fetch_string += "WHERE "
-            fetch_string += "AND ".join(where_clauses)
+            fetch_string += " AND ".join(where_clauses)
+            fetch_string += " "
 
         fetch_string += "ORDER BY RANDOM() LIMIT 1;"
 
@@ -83,7 +100,6 @@ def main() -> int:
                 labels = res.fetchall()
                 for name, in labels:
                     print(name)
-
 
     con.close()
 
